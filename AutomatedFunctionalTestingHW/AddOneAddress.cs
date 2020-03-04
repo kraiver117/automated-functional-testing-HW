e346ae8a4bd64622bb6109d8d046196e7c4bda5d
kraiver117 <31224243+kraiver117@users.noreply.github.com>
2020-03-03 19:00:11 -0600

    Add create user and add Address

---
+++ AutomatedFunctionalTestingHW/AddOneAddress.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutomatedFunctionalTestingHW
+{
+    class AddOneAddress
+    {
+        IWebDriver driver;
+        int seconds = 3000;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new FirefoxDriver("C:/Users/jagutierrez/source/repos/AutomatedFunctionalTestingHW");
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+            driver.Url = "http://automationpractice.com/index.php?controller=authentication&back=my-account";
+
+            Thread.Sleep(seconds);
+            IWebElement loginForm = driver.FindElement(By.Id("email"));
+
+            Actions loginActions = new Actions(driver);
+            loginActions.Click(loginForm)
+                .SendKeys("jagutierrez@gmail.com"+Keys.Tab)
+                .SendKeys("origen117"+Keys.Tab+Keys.Tab)
+                .SendKeys(Keys.Enter)
+                .Build()
+                .Perform();
+
+            AdddingAddress();
+
+
+        }
+
+        private void AdddingAddress()
+        {
+            Thread.Sleep(seconds);
+            driver.FindElement(By.XPath("//li[4]/a/span")).Click();
+
+            driver.FindElement(By.XPath("//div[@id='center_column']/div/a/span")).Click();
+
+            //Filling the form
+            Thread.Sleep(seconds);
+            driver.FindElement(By.Id("firstname")).SendKeys("Angel");
+            driver.FindElement(By.Id("lastname")).SendKeys("Gutierrez");
+            driver.FindElement(By.Id("company")).SendKeys("Scio");
+            driver.FindElement(By.Id("address1")).SendKeys("Corregidora 48");
+            driver.FindElement(By.Id("address2")).SendKeys("Brisa del mar 54");
+            driver.FindElement(By.Id("city")).SendKeys("Morelia");
+            driver.FindElement(By.XPath("//option[5]")).Click();
+            driver.FindElement(By.Id("postcode")).SendKeys("58500");
+            driver.FindElement(By.Id("phone")).SendKeys("434342432");
+            driver.FindElement(By.Id("phone_mobile")).SendKeys("1232143");
+            driver.FindElement(By.Id("other")).SendKeys("Selenium is awesome dude!");
+            driver.FindElement(By.Id("alias")).SendKeys("CJ & BDM");
+            driver.FindElement(By.XPath("//p[2]/button/span")).Click();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            Thread.Sleep(seconds);
+            driver.Close();
+        }
+
+    }
+}
---
+++ AutomatedFunctionalTestingHW/RegistrationTesting.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+
+namespace AutomatedFunctionalTestingHW
+{
+    class RegistrationTesting
+    {
+        IWebDriver driver;
+
+        
+        int seconds = 5000;
+
+
+        [SetUp]
+        public void startBrowser(){
+            driver = new FirefoxDriver("C:/Users/jagutierrez/source/repos/AutomatedFunctionalTestingHW");
+        }
+
+        [Test]
+        public void AutoLogin()
+        {
+            driver.Url = "http://automationpractice.com/index.php";
+
+            //navigating to login page
+            Thread.Sleep(seconds);
+
+            IWebElement loginBtn = driver.FindElement(By.ClassName("login"));
+            loginBtn.Click();
+
+            //fill email register
+            Thread.Sleep(seconds);
+            IWebElement emailAdd = driver.FindElement(By.Id("email_create"));
+            Actions emailActions = new Actions(driver);
+            emailActions.Click(emailAdd)
+                .SendKeys("guti1996@gmail.com" + Keys.Tab)
+                .SendKeys(Keys.Enter)
+                .Build()
+                .Perform();
+
+            //fill inputs
+            Thread.Sleep(seconds);
+            driver.FindElement(By.Id("id_gender1")).Click();
+            driver.FindElement(By.Id("customer_firstname")).SendKeys("Jose");
+            driver.FindElement(By.Id("customer_lastname")).SendKeys("Angel");
+            driver.FindElement(By.Id("passwd")).SendKeys("origen117");
+            driver.FindElement(By.Id("company")).SendKeys("Scio");
+            driver.FindElement(By.Id("address1")).SendKeys("Corregidora #405");
+            driver.FindElement(By.Id("address2")).SendKeys("Colonia Centro");
+            driver.FindElement(By.Id("city")).SendKeys("Puruandiro");
+            driver.FindElement(By.Id("postcode")).SendKeys("58500");
+            driver.FindElement(By.Id("other")).SendKeys("GG Izi");
+            driver.FindElement(By.Id("phone")).SendKeys("234523423");
+            driver.FindElement(By.Id("phone_mobile")).SendKeys("1231231233");
+            driver.FindElement(By.Id("alias")).SendKeys("Kraiver");
+
+
+            //fill selects
+            // Bday
+            Thread.Sleep(3000);
+            IWebElement Bday = driver.FindElement(By.XPath("//option[27]"));
+            Bday.Click();
+            IWebElement Bmonth = driver.FindElement(By.XPath("(//option[@value='4'])[2]"));
+            Bmonth.Click();
+            IWebElement Byear = driver.FindElement(By.XPath("//option[@value='1997']"));
+            Byear.Click();
+
+            //state
+            Thread.Sleep(3000);
+            IWebElement state = driver.FindElement(By.XPath("(//option[@value='3'])[3]"));
+            state.Click();
+
+
+            Thread.Sleep(seconds);
+            IWebElement sendButton = driver.FindElement(By.Id("submitAccount"));
+            sendButton.Click();
+
+
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            Thread.Sleep(seconds);
+            driver.Close();
+        }
+
+
+    }
+}
